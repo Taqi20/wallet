701ad44e04644e9ba3a44366b3ebbe94cf9fde05
Taqi <mohammadtaqi5626@gmail.com>
2025-01-16 04:46:24 +0530

    monorepo init

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,9 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
+import { PrismaClient } from '@repo/db/client';
+const client = new PrismaClient();
+
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
   srcDark: string;
@@ -33,7 +36,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
+            Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
@@ -63,7 +66,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
+        <Button appName="web" className={styles.secondary}>
           Open alert
         </Button>
       </main>
